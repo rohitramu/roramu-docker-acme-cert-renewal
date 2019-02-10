94d5b1a3e5502e6ac4af7db16e57a8aced88fcf8
Rohit Ramu <rohitramu@gmail.com>
2019-02-10 13:31:46 -0800

    Include shebang at the top of the default load and save hook scripts

--- include/load_hook.sh
+++ include/load_hook.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 echo "WARNING: Using default (no-op) load-hook!  The folder that data should be loaded into is: $1"
--- include/save_hook.sh
+++ include/save_hook.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 echo "WARNING: Using default (no-op) save-hook!  The folder which contains the data that should be saved is: $1"
